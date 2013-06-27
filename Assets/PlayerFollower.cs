c121f25f5ee1c1727c03b69c4b55538fbae10ccc
kbarrett <x_kimmi_13_x@hotmail.co.uk>
2013-06-28 00:19:32 +0100

    new player movement method

--- Assets/PlayerFollower.cs
+++ Assets/PlayerFollower.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class PlayerFollower : MonoBehaviour {
 	
-	public float speed;
+	public float maxSpeed;
+    public Vector3 speed = Vector3.zero;
+    float acc = 0.1f;
 	
 	// Use this for initialization
 	void Start () {
@@ -12,24 +14,70 @@ public class PlayerFollower : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        bool inputAccepted = false;
 		if(Input.GetKey(KeyCode.UpArrow))
 		{
-			transform.position = transform.position + new Vector3(0,0,speed);
+            if (speed.z < maxSpeed)
+            {
+                speed.z += acc;
+            }
+            inputAccepted = true;
 		}
 		if(Input.GetKey(KeyCode.DownArrow))
 		{
-			transform.position = transform.position + new Vector3(0,0,-speed);
+            if (speed.z > -maxSpeed)
+            {
+                speed.z -= acc;
+            }
+            inputAccepted = true;
 		}
 		if(Input.GetKey(KeyCode.RightArrow))
 		{
-			transform.position = transform.position + new Vector3(speed,0,0);
+            if (speed.x < maxSpeed)
+            {
+                speed.x += acc;
+            }
+            inputAccepted = true;
 		}
 		if(Input.GetKey(KeyCode.LeftArrow))
 		{
-			transform.position = transform.position + new Vector3(-speed,0,0);
+            if (speed.x > -maxSpeed)
+            {
+                speed.x -= acc;
+            }
+            inputAccepted = true;
 		}
-		
+
+        if (!inputAccepted)
+        {
+            if (speed.x < 0)
+            {
+                speed.x += 2 * acc;
+            }
+            else if (speed.x > 0)
+            {
+                speed.x -= 2*acc;
+            }
+            if (speed.y < 0)
+            {
+                speed.y += 2 * acc;
+            }
+            else if (speed.y > 0)
+            {
+                speed.y -= 2 * acc;
+            }
+            if (speed.z < 0)
+            {
+                speed.z += 2 * acc;
+            }
+            else if (speed.z > 0)
+            {
+                speed.z -= 2 * acc;
+            }
+        }
+
+        transform.Translate(speed);
 	
 		Camera.main.transform.position = transform.position + new Vector3(0,20,0);
 		
